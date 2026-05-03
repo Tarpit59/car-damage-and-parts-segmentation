eb92a83ecd63da6e3fb46bbf059624fd54899469
Tarpit59 <pateltarpit59@gmail.com>
2026-05-03 09:34:20 +0530

    service __init__.py added

---
+++ base/com/service/__init__.py
@@ -0,0 +1,3 @@
+from .output_handler import process_image, render_on_demand
+from .car_parts_segmentation import FINAL_CLASSES as PARTS_CLASSES, PART_COLORS_HEX
+from .car_damage_segmentation import CLASS_NAMES as DAMAGE_CLASSES, DAMAGE_COLORS_HEX
